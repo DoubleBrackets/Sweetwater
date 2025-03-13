fde853409da4d6ac9b3f956c46ca796835def1bb
Arthur G <arthurgan29@gmail.com>
2025-03-13 15:02:38 -0700

    Added shift timer

--- Sweetwater_Unity/Assets/Scripts/SquidMinigame/FishingShiftTimer.cs
+++ Sweetwater_Unity/Assets/Scripts/SquidMinigame/FishingShiftTimer.cs
@@ -1,17 +1,50 @@
+using NaughtyAttributes;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace SquidMinigame
 {
     public class FishingShiftTimer : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        [Header("Depends")]
+
+        [SerializeField]
+        private Slider _shiftTimerSlider;
+
+        [SerializeField]
+        private TMP_Text _timeRemainingText;
+
+        [Header("Config")]
+
+        [SerializeField]
+        private float _shiftDuration;
+
+        [Header("Events")]
+
+        public UnityEvent OnShiftEnd;
+
+        [ShowNonSerializedField]
+        private float _timeLeft;
+
         private void Start()
         {
+            _timeLeft = _shiftDuration;
         }
 
-        // Update is called once per frame
         private void Update()
         {
+            if (_timeLeft <= 0)
+            {
+                OnShiftEnd?.Invoke();
+                return;
+            }
+
+            _timeLeft -= Time.deltaTime;
+            _shiftTimerSlider.normalizedValue = _timeLeft / _shiftDuration;
+            int minutesLeft = Mathf.CeilToInt(_timeLeft / 60);
+            _timeRemainingText.text = $"{minutesLeft}";
         }
     }
 }
